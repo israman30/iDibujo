813d32542dab2c47486fae4f6f8f1b7bfa994f43
Israel Manzo <israelmanzo814@gmail.com>
2019-11-04 21:45:03 -0500

    Adding extra commnet to draw class

--- iDibujo/iDibujo/Model/Drawing+Ext.swift
+++ iDibujo/iDibujo/Model/Drawing+Ext.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
+// MARK: - THIS SECTION DRAW THE CANVAS
 extension MainController {
     
+    // MARK: - TOUCHES BEGAN ON VIEW METHOD, get first touch and assigned to the last location in the view.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         swipe = false
         if let touch = touches.first {
@@ -27,24 +29,27 @@ extension MainController {
      */
     func drawnLine(from: CGPoint, to: CGPoint) {
         UIGraphicsBeginImageContext(self.view.frame.size)
+        // Get container and assign it as canvas
         container.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-        
+        // Get current graphic context
         let context = UIGraphicsGetCurrentContext()
+        // move the pointer method from x to y
         context?.move(to: CGPoint(x: from.x, y: from.y))
+        // add line to the context when x and y moves
         context?.addLine(to: CGPoint(x: to.x, y: to.y))
         
         context?.setBlendMode(.normal)
-        context?.setLineCap(CGLineCap.round)
+        context?.setLineCap(CGLineCap.round) // give round to the line
         context?.setLineWidth(brushSize) // Brush size variable value
-        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacity).cgColor)
-        context?.strokePath()
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacity).cgColor) // set stroke to the context pointer
+        context?.strokePath() // set path to the context pointer
         
         container.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+        UIGraphicsEndImageContext() // ends graphic context
     }
     
+    // MARK: - TOUCHES MOVES, check if the first otuch exist then draw a line from the last point to the current position. Then assign it to the last point.
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         swipe = true
         if let touch = touches.first {
             let currentPoint = touch.location(in: self.view)
@@ -53,6 +58,7 @@ extension MainController {
         }
     }
     
+    // MARK: - TOUCHES END, joins the first with last point.
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if !swipe {
             drawnLine(from: lastPoint, to: lastPoint)
