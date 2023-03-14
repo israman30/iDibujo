5cbad97534085fde9e2967bbfe9d4efd67938579
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-03-14 10:36:21 -0400

    Revert "updated fonts for main canvas"
    This reverts commit 26c75fa84e4a44b50ccf67bc1d966d5fc55df45e.

--- iDibujo/iDibujo/View/Anchors+Ext.swift
+++ iDibujo/iDibujo/View/Anchors+Ext.swift
@@ -11,7 +11,7 @@ import UIKit
 // MARK: - Custom button for each BUTTON color
 class CustomButton: UIButton {
     
-    convenience init(for title: String, titleColor: UIColor, background: UIColor, fontSize: CGFloat, font: UIFont) {
+    convenience init(for title: String, titleColor: UIColor, background: UIColor, fontSize: CGFloat) {
         self.init()
         setTitle(title, for: .normal)
         setTitleColor(titleColor, for: .normal)
@@ -22,7 +22,6 @@ class CustomButton: UIButton {
         titleLabel?.isAccessibilityElement = true
         layer.borderWidth = 3
         layer.borderColor = UIColor.black.cgColor
-        titleLabel?.font = font
     }
 }
 
