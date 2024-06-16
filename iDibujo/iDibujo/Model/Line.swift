0b242cf8ca7f0784feb89e4970ce9a59b0b7d888
Israel Manzo <israelmanzo814@gmail.com>
2024-06-16 11:44:25 -0400

    created separated component for canvas view

---
+++ iDibujo/iDibujo/Model/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  iDibujo
+//
+//  Created by Israel Manzo on 6/16/24.
+//  Copyright © 2024 Israel Manzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct Line {
+    var points: [CGPoint]
+    var color: Color
+}
+
