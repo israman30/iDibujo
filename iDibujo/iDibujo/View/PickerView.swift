7669a83307229df965d4f143ad2a5cf4cc149275
Israel Manzo <israelmanzo814@gmail.com>
2025-02-20 20:57:39 -0500

    adding some spacing

--- iDibujo/iDibujo/View/PickerView.swift
+++ iDibujo/iDibujo/View/PickerView.swift
@@ -13,14 +13,12 @@ struct PickerView: View {
     @State var mixer: Double = 0
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             ColorPicker("PickColor", selection: $colorPicked)
                 .labelsHidden()
             RoundedRectangle(cornerRadius: 5)
                 .fill(colorPicked)
                 .frame(width: 100, height: 100)
-            
-            Text("\((1 - mixer), format: .percent.precision(.fractionLength(0...2)))")
         }
     }
 }
