32f520b58057585c328c66faa0600fc1c094aac2
Israel Manzo <israelmanzo814@gmail.com>
2025-03-10 17:23:26 -0400

    updated canvas background color for

--- iDibujo/iDibujo/View/MainCanvas.swift
+++ iDibujo/iDibujo/View/MainCanvas.swift
@@ -15,6 +15,8 @@ struct MainCanvas: View {
     
     var body: some View {
         ZStack {
+            Color.white
+                .ignoresSafeArea()
             CanvasView(
                 lines: $lineViewModel.lines,
                 selectedColor: $lineViewModel.selectedColor, vm: lineViewModel
