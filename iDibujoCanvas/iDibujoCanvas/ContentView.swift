6ad6035e073d158050f789981cb58028f0a8d2ba
Israel Manzo <israelmanzo814@gmail.com>
2025-04-25 12:18:40 -0400

    Added a new project for canvas

---
+++ iDibujoCanvas/iDibujoCanvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDibujoCanvas
+//
+//  Created by Israel Manzo on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDibujoCanvas/iDibujoCanvas/iDibujoCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDibujoCanvasApp.swift
+//  iDibujoCanvas
+//
+//  Created by Israel Manzo on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct iDibujoCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
