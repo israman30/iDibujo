4d67da714a8d929b9ad9b9562224747a43a710a9
Israel Manzo <israelmanzo814@gmail.com>
2025-04-25 14:13:59 -0400

    added canvas engine

--- iDibujoCanvas/iDibujoCanvas/ContentView.swift
+++ iDibujoCanvas/iDibujoCanvas/ContentView.swift
@@ -29,10 +29,15 @@ struct CanvasView: View {
     @State private var selectedColor: Color = .black
     @State private var selectedWidth: CGFloat = 1
     
+    let canvas = CanvasEnginer()
+    
     var body: some View {
         VStack {
             VStack {
                 ColorPicker("line color", selection: $selectedColor)
+                Button("Delete") {
+                    lines = []
+                }
                 HStack {
                     Slider(value: $selectedWidth, in: 1...20) {
                         Text("line width: \($selectedWidth)")
@@ -44,10 +49,8 @@ struct CanvasView: View {
             
             Canvas { context, size in
                 for line in lines {
-                    var path = Path()
-                    path.addLines(line.points)
-                    
-                    context.stroke(path, with: .color(line.color), lineWidth: line.width)
+                    let path = canvas.createPath(for: line.points)
+                    context.stroke(path, with: .color(line.color), style: StrokeStyle(lineWidth: line.width, lineCap: .round, lineJoin: .round))
                 }
             }
             .gesture(
@@ -68,5 +71,21 @@ struct CanvasView: View {
                 })
             )
         }
+    }
+}
+
+class CanvasEnginer {
+    
+    func createPath(for points: [CGPoint]) -> Path {
+        var path = Path()
+        path.move(to: points[0])
+        for point in points[1...] {
+            path.addLine(to: point)
+        }
+        return path
+    }
+    
+    func calculate(_ point1: CGPoint, point2: CGPoint) -> CGPoint {
+        CGPoint(x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2)
     }
 }
