141f36df6f7b1ec8251b7e02298bc39182d523bf
Israel Manzo <israelmanzo814@gmail.com>
2025-04-25 12:29:12 -0400

    added canvas view and constructing lines array

--- iDibujoCanvas/iDibujoCanvas/ContentView.swift
+++ iDibujoCanvas/iDibujoCanvas/ContentView.swift
@@ -7,18 +7,49 @@
 
 import SwiftUI
 
+struct Line {
+    var points: [CGPoint]
+    var color: Color
+    var width: CGFloat
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CanvasView()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CanvasView: View {
+    
+    @State private var lines = [Line]()
+    
+    var body: some View {
+        Canvas { context, size in
+            for line in lines {
+                var path = Path()
+                path.addLines(line.points)
+                
+                context.stroke(path, with: .color(line.color), lineWidth: line.width)
+            }
+        }
+        .gesture(
+            DragGesture(
+                minimumDistance: 0,
+                coordinateSpace: .local
+            ).onChanged({ value in
+                let newPoint = value.location
+                if value.translation.width + value.translation.height == 0 {
+                    // TODO: implement color
+                    lines.append(Line(points: [newPoint], color: .blue, width: 5))
+                } else {
+                    let index = lines.count - 1
+                    lines[index].points.append(newPoint)
+                }
+            })
+        )
+    }
 }
