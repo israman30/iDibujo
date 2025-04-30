a86b5eb9925cc2a60200fe9c0ce0d07d980e180f
Israel Manzo <israelmanzo814@gmail.com>
2025-04-29 22:57:42 -0400

    added new project for iDibujo for mac

---
+++ iDibujo Mac/iDibujo Mac/iDibujo_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDibujo_MacApp.swift
+//  iDibujo Mac
+//
+//  Created by Israel Manzo on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct iDibujo_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
