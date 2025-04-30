1a0c4c1bebf36341be7e355fe7eacfeddb3d828c
Israel Manzo <israelmanzo814@gmail.com>
2025-04-29 23:00:16 -0400

    added model line

---
+++ iDibujo Mac/iDibujo Mac/Model/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  iDibujo Mac
+//
+//  Created by Israel Manzo on 4/29/25.
+//
+
+import SwiftUI
+
+struct Line {
+    var points: [CGPoint]
+    var color: Color
+    var lineWidth: CGFloat
+}
