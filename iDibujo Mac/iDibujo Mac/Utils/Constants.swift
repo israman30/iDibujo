e93f2e28734c06e39a0193532d8c152681fad7ad
Israel Manzo <israelmanzo814@gmail.com>
2025-04-30 09:52:41 -0400

    update constant file

--- iDibujo Mac/iDibujo Mac/Utils/Constants.swift
+++ iDibujo Mac/iDibujo Mac/Utils/Constants.swift
@@ -4,24 +4,7 @@
 //
 //  Created by Israel Manzo on 4/30/25.
 //
-
-import UIKit
-import SwiftUI
-
-struct CustomColor {
-    static let blackColor = UIColor(Color(.label))
-    static let blackPrimary = UIColor.black
-    static let blackSecondary = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
-    
-    static let redPrimary = UIColor.red
-    static let redSecondary = UIColor(red: 178/255, green: 34/255, blue: 34/255, alpha: 1)
-    
-    static let greenPrimary = UIColor.green
-    static let greenSecondary = UIColor(red: 50/255, green: 205/255, blue: 50/255, alpha: 1)
-    
-    static let bluePrimary = UIColor.blue
-    static let blueSecondary = UIColor(red: 0/255, green: 0/255, blue: 125/255, alpha: 1)
-}
+import Foundation
 
 struct CustomIcon {
     static let tray_and_arrow_down = "tray.and.arrow.down"
