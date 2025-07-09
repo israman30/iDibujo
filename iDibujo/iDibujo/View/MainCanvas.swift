6f55c94e3eed5bdf310464d8cbde6ad27072e84a
Israel Manzo <israelmanzo814@gmail.com>
2025-07-09 16:29:42 -0400

    update orientation support

--- iDibujo/iDibujo/View/MainCanvas.swift
+++ iDibujo/iDibujo/View/MainCanvas.swift
@@ -12,6 +12,7 @@ struct MainCanvas: View {
     @State private var colorPicked: Color = .blue
     @State private var alert: AlertComponent? = nil
     @StateObject private var lineViewModel = LineViewModel()
+    @Environment(\.verticalSizeClass) private var orientation
     
     var body: some View {
         ZStack {
@@ -62,20 +63,47 @@ struct MainCanvas: View {
     
     @ViewBuilder
     func menuList() -> some View {
-        VStack {
-            MenuButtonView(lineViewModel: lineViewModel, savePhotoLibrary: {
-                savePhotoLibrary()
-            }, presentAlert: {
-                presentAlert()
-            })
-            .presentAlert($alert, isPresented: $showingAlert)
-            
-            ColorPicker(Labels.colorPicker, selection: $lineViewModel.selectedColor)
-                .labelsHidden()
-            
-            SliderView(value: $lineViewModel.lineWithValue)
-            
-            UndoRedoButtonView(lineViewModel: lineViewModel)
+        if orientation == .regular {
+            VStack {
+                // Menu button view
+                MenuButtonView(lineViewModel: lineViewModel, savePhotoLibrary: {
+                    savePhotoLibrary()
+                }, presentAlert: {
+                    presentAlert()
+                })
+                .presentAlert($alert, isPresented: $showingAlert)
+                
+                // Color Picker View
+                ColorPicker(Labels.colorPicker, selection: $lineViewModel.selectedColor)
+                    .labelsHidden()
+                // Slider View
+                SliderView(value: $lineViewModel.lineWithValue)
+                // Redo buttons
+                UndoRedoButtonView(lineViewModel: lineViewModel)
+            }
+        } else {
+            VStack {
+                HStack(spacing: 15) {
+                    // Color Picker View
+                    ColorPicker(Labels.colorPicker, selection: $lineViewModel.selectedColor)
+                        .labelsHidden()
+                    // Redo buttons
+                    UndoRedoButtonView(lineViewModel: lineViewModel)
+                    // Menu button view
+                    MenuButtonView(lineViewModel: lineViewModel, savePhotoLibrary: {
+                        savePhotoLibrary()
+                    }, presentAlert: {
+                        presentAlert()
+                    })
+                    .presentAlert($alert, isPresented: $showingAlert)
+                }
+                   
+                HStack {
+                    Spacer()
+                    // Slider View
+                    SliderView(value: $lineViewModel.lineWithValue)
+                }
+            }
         }
     }
     
@@ -142,9 +170,47 @@ struct MenuButtonView: View {
 
 struct UndoRedoButtonView: View {
     @ObservedObject var lineViewModel: LineViewModel
+    @Environment(\.verticalSizeClass) private var orientation
     
     var body: some View {
+        if orientation == .regular {
+            redoButtonPortraitOrientation
+        } else {
+            redoButtonLandscapeOrientation
+        }
+    }
+    
+    @ViewBuilder
+    private var redoButtonPortraitOrientation: some View {
         VStack(spacing: 20) {
+            /// `undo` Button
+            Button {
+                let lastLine = lineViewModel.lines.removeLast()
+                lineViewModel.deletedLines.append(lastLine)
+            } label: {
+                Image(systemName: CustomIcon.undo)
+                    .font(.title3)
+                    .fontWeight(.heavy)
+            }
+            .disabled(lineViewModel.lines.count == 0)
+            .if(lineViewModel.lines.count == 0)
+            /// `redo` Buton
+            Button {
+                let lastLine = lineViewModel.deletedLines.removeLast()
+                lineViewModel.lines.append(lastLine)
+            } label: {
+                Image(systemName: CustomIcon.redo)
+                    .font(.title3)
+                    .fontWeight(.heavy)
+            }
+            .disabled(lineViewModel.deletedLines.count == 0)
+            .if(lineViewModel.deletedLines.count == 0)
+        }
+    }
+    
+    @ViewBuilder
+    private var redoButtonLandscapeOrientation: some View {
+        HStack(spacing: 20) {
             /// `undo` Button
             Button {
                 let lastLine = lineViewModel.lines.removeLast()
